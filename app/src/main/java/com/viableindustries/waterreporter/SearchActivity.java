15540376b5824cade90edf1166ed20771c950624
brendandm <brendan.david.mcintyre@gmail.com>
2017-02-14 17:19:25 -0500

    Added dynamic filtering to search UI

--- app/src/main/java/com/viableindustries/waterreporter/SearchActivity.java
+++ app/src/main/java/com/viableindustries/waterreporter/SearchActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -14,7 +16,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v4.widget.TextViewCompat;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -45,6 +49,7 @@ import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
@@ -54,6 +59,9 @@ public class SearchActivity extends FragmentActivity {
 
     @Bind(R.id.search_box)
     EditText searchBox;
+
+    @Bind(R.id.clear_search)
+    ImageButton clearSearch;
 
     @Bind(R.id.search_people)
     Button searchPeople;
@@ -78,9 +86,17 @@ public class SearchActivity extends FragmentActivity {
 
     protected UserListAdapter userListAdapter;
 
+    ArrayList<Organization> baseOrganizationList;
+
+    ArrayList<Organization> orgMatches;
+
+    ArrayList<User> baseUserList;
+
+    ArrayList<User> userMatches;
+
 //    ListView resultList;
 
-    private int SEARCH_ACTION = 0;
+    private int activeTab = 0;
 
     private String query;
 
@@ -187,7 +203,20 @@ public class SearchActivity extends FragmentActivity {
 //
 //    }
 
-    private String buildQuery(String sortField, String sortDirection, Map filterArgs) {
+    // Observe changes in search input
+
+//    @OnTextChanged(R.id.search_box)
+//    void onSearchTextChanged(CharSequence q, int start, int count, int after) {
+//
+//        query = q.toString();
+//
+//        fetchUsers(10, 1, buildQuery("user", "last_name", "asc", query), true);
+//
+//        fetchOrganizations(10, 1, buildQuery("organization", "name", "asc", query), false);
+//
+//    }
+
+    private String buildQuery(String collection, String sortField, String sortDirection, String searchChars) {
 
         // Create order_by list and add a sort parameter
 
@@ -200,6 +229,32 @@ public class SearchActivity extends FragmentActivity {
         // Create filter list and add a filter parameter
 
         List<QueryFilter> queryFilters = new ArrayList<QueryFilter>();
+
+        if ("user".equals(collection)) {
+
+            QueryFilter userPictureFilter = new QueryFilter("picture", "is_not_null", null);
+
+            queryFilters.add(userPictureFilter);
+
+            if (searchChars != null) {
+
+                QueryFilter userFirstNameFilter = new QueryFilter("last_name", "ilike", String.format("%s%s", searchChars, "%"));
+
+                queryFilters.add(userFirstNameFilter);
+
+            }
+
+        } else if ("organization".equals(collection)) {
+
+            if (searchChars != null) {
+
+                QueryFilter orgNameFilter = new QueryFilter("name", "ilike", String.format("%s%s", searchChars, "%"));
+
+                queryFilters.add(orgNameFilter);
+
+            }
+
+        }
 
 //        QueryFilter complexVal = new QueryFilter("id", "eq", organization.id);
 //
@@ -215,10 +270,7 @@ public class SearchActivity extends FragmentActivity {
 
     }
 
-    protected void fetchOrganizations(int limit, int page, final String query) {
-
-//        final SharedPreferences prefs =
-//                context.getSharedPreferences(context.getPackageName(), MODE_PRIVATE);
+    protected void fetchOrganizations(int limit, int page, final String query, final boolean filterResults, final boolean switchCollection) {
 
         final String accessToken = prefs.getString("access_token", "");
 
@@ -235,15 +287,31 @@ public class SearchActivity extends FragmentActivity {
 
                 ArrayList<Organization> organizations = organizationFeatureCollection.getFeatures();
 
+//                searchAction = switchCollection ? 1 : searchAction;
+
                 if (!organizations.isEmpty()) {
 
-//                    memberCollection.addAll(users);
+                    if (!filterResults) {
 
-                    orgListAdapter = new OrganizationListAdapter(SearchActivity.this, organizations, true);
+                        baseOrganizationList.addAll(organizations);
 
-                    searchResults.setAdapter(orgListAdapter);
+                        orgListAdapter = new OrganizationListAdapter(SearchActivity.this, baseOrganizationList, true);
 
-//                    attachScrollListener();
+                    } else {
+
+                        orgListAdapter = new OrganizationListAdapter(SearchActivity.this, organizations, true);
+
+                    }
+
+//                    baseOrganizationList.addAll(organizations);
+//
+//                    orgListAdapter = new OrganizationListAdapter(SearchActivity.this, organizations, true);
+
+                    if (switchCollection || activeTab == 1) {
+
+                        searchResults.setAdapter(orgListAdapter);
+
+                    }
 
                 }
 
@@ -276,10 +344,7 @@ public class SearchActivity extends FragmentActivity {
 
     }
 
-    protected void fetchUsers(int limit, int page, final String query) {
-
-//        final SharedPreferences prefs =
-//                context.getSharedPreferences(context.getPackageName(), MODE_PRIVATE);
+    protected void fetchUsers(int limit, int page, final String query, final boolean filterResults, final boolean switchCollection) {
 
         final String accessToken = prefs.getString("access_token", "");
 
@@ -296,15 +361,27 @@ public class SearchActivity extends FragmentActivity {
 
                 ArrayList<User> users = userCollection.getFeatures();
 
+//                searchAction = switchCollection ? 0 : searchAction;
+
                 if (!users.isEmpty()) {
 
-//                    memberCollection.addAll(users);
+                    if (!filterResults) {
 
-                    userListAdapter = new UserListAdapter(SearchActivity.this, users, true);
+                        baseUserList.addAll(users);
 
-                    searchResults.setAdapter(userListAdapter);
+                        userListAdapter = new UserListAdapter(SearchActivity.this, baseUserList, true);
 
-//                    attachScrollListener();
+                    } else {
+
+                        userListAdapter = new UserListAdapter(SearchActivity.this, users, true);
+
+                    }
+
+                    if (switchCollection || activeTab == 0) {
+
+                        searchResults.setAdapter(userListAdapter);
+
+                    }
 
                 }
 
@@ -348,13 +425,41 @@ public class SearchActivity extends FragmentActivity {
 
         prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
 
-        fetchUsers(10, 1, buildQuery("last_name", "asc", null));
+        // Initialize empty list to hold organizations
+
+        baseOrganizationList = new ArrayList<Organization>();
+
+        // Initialize empty list to hold users
+
+        baseUserList = new ArrayList<User>();
+
+        fetchUsers(10, 1, buildQuery("user", "last_name", "asc", null), false, true);
+
+        fetchOrganizations(10, 1, buildQuery("organization", "name", "asc", null), false, false);
 
         searchPeople.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                fetchUsers(10, 1, buildQuery("last_name", "asc", null));
+                searchResults.setAdapter(null);
+
+                Log.d("Switch tab", "users");
+
+                if (baseUserList.isEmpty()) {
+
+                    Log.d("Switch tab", "User list is empty");
+
+                    fetchUsers(10, 1, buildQuery("user", "last_name", "asc", null), false, true);
+
+                } else {
+
+                    Log.d("Switch tab", "User list not empty");
+
+                    userListAdapter = new UserListAdapter(SearchActivity.this, baseUserList, true);
+
+                    searchResults.setAdapter(userListAdapter);
+
+                }
 
             }
         });
@@ -363,10 +468,116 @@ public class SearchActivity extends FragmentActivity {
             @Override
             public void onClick(View v) {
 
-                fetchOrganizations(10, 1, buildQuery("name", "asc", null));
+                searchResults.setAdapter(null);
+
+                activeTab = 1;
+
+                Log.d("Switch tab", "orgs");
+
+                if (baseOrganizationList.isEmpty()) {
+
+                    Log.d("Switch tab", "Org list is empty");
+
+                    fetchOrganizations(10, 1, buildQuery("organization", "name", "asc", null), false, true);
+
+                } else {
+
+                    Log.d("Switch tab", "Org list not empty");
+
+                    orgListAdapter = new OrganizationListAdapter(SearchActivity.this, baseOrganizationList, true);
+
+                    searchResults.setAdapter(orgListAdapter);
+
+                }
 
             }
         });
+
+        clearSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //query = null;
+
+                searchBox.setText("");
+
+            }
+        });
+
+        // Observe changes in search input and respond accordingly
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+
+        final Runnable userSearchRunnable = new Runnable() {
+            @Override
+            public void run() {
+                fetchUsers(10, 1, buildQuery("user", "last_name", "asc", query), true, true);
+            }
+        };
+
+        final Runnable orgSearchRunnable = new Runnable() {
+            @Override
+            public void run() {
+                fetchOrganizations(10, 1, buildQuery("organization", "name", "asc", query), true, false);
+            }
+        };
+
+        searchBox.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+//                seq = cs;
+
+                query = cs.toString();
+
+//                fetchUsers(10, 1, buildQuery("user", "last_name", "asc", query), true);
+//
+//                fetchOrganizations(10, 1, buildQuery("organization", "name", "asc", query), false);
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int arg1, int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+//                new SearchTask().execute(seq.toString().trim());
+//
+//                query = q.toString();
+
+                handler.removeCallbacks(userSearchRunnable);
+
+                handler.removeCallbacks(orgSearchRunnable);
+
+//                workRunnable = () -> fetchUsers(10, 1, buildQuery("user", "last_name", "asc", query), true);
+
+                switch (activeTab) {
+
+                    case 0:
+
+                        handler.postDelayed(userSearchRunnable, 300 /*delay*/);
+
+                        break;
+
+                    case 1:
+
+                        handler.postDelayed(orgSearchRunnable, 300 /*delay*/);
+
+                        break;
+
+                }
+
+            }
+
+        });
+
+//        @Override public void afterTextChanged(Editable s) {
+//            handler.removeCallbacks(workRunnable);
+//            workRunnable = () -> doSmth(s.toString());
+//            handler.postDelayed(workRunnable, 500 /*delay*/);
+//        }
 
 
         // Instantiate a ViewPager and a PagerAdapter.
