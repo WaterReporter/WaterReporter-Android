f12c7bae53dc80398a74c6123e4d3d2ff4de5a14
brendandm <brendan.david.mcintyre@gmail.com>
2018-03-26 11:02:54 -0400

    Added components for campaign group, member and watershed views

---
+++ app/src/main/java/com/viableindustries/waterreporter/CampaignMembersActivity.java
@@ -0,0 +1,14 @@
+package com.viableindustries.waterreporter;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CampaignMembersActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign_members);
+    }
+
+}
---
+++ app/src/main/java/com/viableindustries/waterreporter/CampaignWatershedsActivity.java
@@ -0,0 +1,14 @@
+package com.viableindustries.waterreporter;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CampaignWatershedsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign_watersheds);
+    }
+
+}
